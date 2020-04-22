49c73c0938eae4d72503cb849721b578a4a732cd
Justin Kusz <tma1.spaceflunky@gmail.com>
2020-04-22 16:52:36 -0400

    added last.fm artist lookup

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ var logger = require("winston");
 var fetch = require("node-fetch");
 var auth = process.env.NODE_ENV === "dev" ? require("./auth.json") : null;
 var he = require("he");
+var sanitize = require("sanitize-html");
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -11,12 +12,20 @@ logger.add(new logger.transports.Console(), {
 });
 logger.level = "debug";
 
+// Set some constants
 const bot_token =
-  process.env.NODE_ENV === "dev" ? auth.discord_bot_token : process.env.discord_bot_token;
+  process.env.NODE_ENV === "dev"
+    ? auth.discord_bot_token
+    : process.env.discord_bot_token;
 const red_api_key =
   process.env.NODE_ENV === "dev" ? auth.red_api_key : process.env.red_api_key;
 const red_url =
   process.env.NODE_ENV === "dev" ? auth.red_url : process.env.red_url;
+const lastfm_api_key =
+  process.env.NODE_ENV === "dev"
+    ? auth.lastfm_api_key
+    : process.env.lastfm_api_key;
+const lastfm_url = "https://ws.audioscrobbler.com/2.0/";
 
 // Initialize Discord Bot
 var bot = new Discord.Client({
@@ -34,39 +43,48 @@ bot.on("message", function (user, userID, channelID, message, evt) {
   // Our bot needs to know if it will execute a command
   // It will listen for messages that will start with `!red`
   try {
-    if (message.substring(0, 4) === "!red") {
-      if (message.length === 4) {
-        sendDefaultMsg(channelID);
-        return;
-      }
-
-      if (message.substring(4, 5) !== " ") {
-        return;
-      }
-
+    if (message.substring(0, 1) === "!") {
+      var cmd = message.split(" ")[0];
       var args = message.split(" ").slice(1);
-      var cmd = args[0];
-      var query = args.slice(1).join(" ");
 
       switch (cmd) {
-        case "top":
-          getTopTen(query).then((message) => {
+        case "!artist":
+          var artist = args.join(" ");
+          getLastFMArtistInfo(artist).then((message) => {
             bot.sendMessage({
               to: channelID,
               message,
             });
           });
           break;
-        case "like":
-          getSimilarArtists(query).then((message) => {
-            bot.sendMessage({
-              to: channelID,
-              message,
-            });
-          });
+        case "!red":
+          var query = args[0];
+          var params = args.slice(1);
+
+          switch (query) {
+            case "top":
+              var period = params[0];
+              getTopTen(period).then((message) => {
+                bot.sendMessage({
+                  to: channelID,
+                  message,
+                });
+              });
+              break;
+            case "like":
+              var artist = params.join(" ");
+              logger.info(artist);
+              getSimilarArtists(artist).then((message) => {
+                bot.sendMessage({
+                  to: channelID,
+                  message,
+                });
+              });
+              break;
+            default:
+              sendDefaultMsg(channelID);
+          }
           break;
-        default:
-          sendDefaultMsg(channelID);
       }
     }
   } catch (error) {
@@ -105,7 +123,14 @@ var getSimilarArtists = async (artist) => {
     }
 
     const name = he.decode(data.response.name);
-    const similarArtists = data.response.similarArtists
+
+    var similarArtists = data.response.similarArtists;
+
+    if (similarArtists.length === 0) {
+      return `Wow. There are no similar artists to \`${name}\`. They are truly unique.`;
+    }
+
+    similarArtists = similarArtists
       .map((artist) => `* ${he.decode(artist.name)}`)
       .join("\n");
 
@@ -180,4 +205,38 @@ var formattedResult = (index, result) => {
   const tags = result.tags.join(", ");
 
   return `${index}. ${artist} - ${album} [${year}]\n< ${tags} >`;
+};
+
+var getLastFMArtistInfo = async (artist) => {
+  if (!artist) {
+    return "**Usage:**\n`!artist pink floyd`";
+  }
+
+  const url =
+    lastfm_url +
+    `?method=artist.getinfo&artist=${artist}&api_key=${lastfm_api_key}&format=json`;
+
+  try {
+    var response = await fetch(url);
+    var data = await response.json();
+
+    if (data.error) {
+      if (data.error === 6) {
+        return `Oops! I couldn't find an arist named \`${artist}\`, ya dingus.`;
+      } else {
+        return `Oops! Something's fucky. Did you *break* me?!`;
+      }
+    }
+
+    var name = data.artist.name;
+    var artistUrl = data.artist.url;
+    var bio = sanitize(data.artist.bio.summary, { allowedTags: [] });
+    var tags = data.artist.tags.tag.map((tag) => tag.name).join(", ");
+
+    var message = `**Info for \`${name}\`**\nFrom Last.fm: <${artistUrl}>\`\`\`markdown\n${bio}\n\n< ${tags} >\`\`\``;
+
+    return message;
+  } catch (error) {
+    logger.error(error.message);
+  }
 };
